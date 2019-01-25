08600cc624cb4159b3cfb8bb8367dac24413020c
Scott Haleen <scott.haleen@gmail.com>
2019-01-24 20:01:26 -0500

    finally created a lein-plugin

--- project.clj
+++ project.clj
@@ -2,20 +2,14 @@
 
 (defproject spring-cloud "0.1.0-SNAPSHOT"
   :description ""
-  :dependencies [
-                 [org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.springframework.boot/spring-boot-starter-web ~SPRING-BOOT-VERSION
                   :exclusions [org.springframework.boot/spring-boot-starter-tomcat]]
                  [org.springframework.boot/spring-boot-starter-jetty ~SPRING-BOOT-VERSION]
-                 [org.springframework.boot/spring-boot-starter-actuator ~SPRING-BOOT-VERSION]
-                 ;;[org.springframework.boot/spring-boot-loader "1.4.3.RELEASE"]
-                 ]
+                 [org.springframework.boot/spring-boot-starter-actuator ~SPRING-BOOT-VERSION]]
   :repositories [["spring-milestone" "https://repo.spring.io/milestone"]]
-  :main org.springframework.boot.loader.JarLauncher
-  :manifest {"Start-Class" "spring.cloud.App"
-             "Spring-Boot-Classes" "BOOT-INF/classes/"
-             "Spring-Boot-Lib" "BOOT-INF/lib/"}
-  :libdir-path "target/spring-boot/BOOT-INF/lib"
-  :plugins [[lein-libdir "0.1.1"]
+  :main spring.cloud.App
+  :spring-boot-loader-version "2.1.2.RELEASE"
+  :plugins [[lein-spring-boot-jar "0.1.0"]
             [lein-ancient "0.6.15"]]
   :aot :all)
